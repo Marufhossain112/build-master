4d6fe088cb532170c5f7d5b1b3bbba93e47e0d72
maruf <dev.maruf112@gmail.com>
2023-08-19 14:14:56 +0600

    [Done] data created for featured products

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ export default function Navbar() {
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
                     <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                        <li><a>Home</a></li>
+                        <Link href="/"><li><a>Home</a></li></Link>
                         <li><a>Sign in</a></li>
                         <li><a>Sign out</a></li>
                         <li><a>Sign up</a></li>
@@ -19,25 +19,25 @@ export default function Navbar() {
                             <details>
                                 <summary>Categories</summary>
                                 <ul className="p-2">
-                                    <li><a>CPU</a></li>
-                                    <li><a>Motherboard</a></li>
-                                    <li><a>RAM</a></li>
-                                    <li><a>Power Supply</a></li>
-                                    <li><a>Storage
-                                        Device</a></li>
-                                    <li><a>Monitor</a></li>
-                                    <li><a>Others</a></li>
+                                    <li><Link href="/pc/parts/cpu">CPU</Link></li>
+                                    <li><Link href="/pc/parts/motherboard">Motherboard</Link></li>
+                                    <li><Link href="/pc/parts/ram">RAM</Link></li>
+                                    <li><Link href="/pc/parts/power-supply">Power Supply</Link></li>
+                                    <li><Link href="/pc/parts/storage-device">Storage
+                                        Device</Link></li>
+                                    <li><Link href="/pc/parts/others">Others</Link></li>
                                 </ul>
                             </details>
                         </li>
                     </ul>
                 </div>
+                {/* <Link href="/" >  </Link>  */}
                 <Image className='ml-4' src={logo} alt='logo' height={30} width={30} />
-                <a className="btn btn-ghost normal-case text-xl">Build Master</a>
+                <Link href="/" className="btn btn-ghost normal-case text-xl">Build Master</Link>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a>Home</a></li>
+                    <Link href="/"><li><a>Home</a></li></Link>
                     <li><a>Sign in</a></li>
                     <li><a>Sign out</a></li>
                     <li><a>Sign up</a></li>

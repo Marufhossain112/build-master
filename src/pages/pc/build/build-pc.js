646beb938ff2fa9e6bbf6f9608033657c90b7e2b
Maruf Hossain <108384673+Marufhossain112@users.noreply.github.com>
2023-11-28 12:35:03 +0600

    pc builder fix

--- src/pages/pc/build/build-pc.js
+++ src/pages/pc/build/build-pc.js
@@ -1,71 +1,95 @@
-import RootLayout from '@/Layouts/RootLayout';
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { toast } from 'react-toastify';
+import RootLayout from "@/Layouts/RootLayout";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { useSelector } from "react-redux";
+import { toast } from "react-toastify";
 
 export default function PcBuild({ categoryData }) {
-    const selectedData = useSelector(state => state.persistedPcBuilderReducer.selectedProduct);
-    // console.log("selected data", selectedData);
-    // console.log("categoryData", categoryData);
-    const handleCompleteBuild = () => toast('Build is completed successfully', { hideProgressBar: true, autoClose: 2000, type: 'success', closeButton: false });
-    return (
-        <div className=''>
-            <h1 className='text-center text-xl font-semibold my-5'>Components</h1>
-            <div className='flex gap-2 flex-col mx-1'>
-                {categoryData && categoryData.map((category, index) => (
-                    <div key={index} className="parent  border border-gray-400 rounded-sm p-2 w-full  md:w-[50%]  mx-auto flex justify-between overflow-auto">
-                        {/* category logo and name */}
-                        <div className='flex gap-1 max-w-md max-h-16'>
-                            <Image src={category.image} width={48} height={48} alt='img' />
-                            <h5>{category.name}</h5>
-                        </div>
-                        <div>
-                            {selectedData.map((product, index) => (
-                                <div key={index}>{product.category === category.name ? <>
-                                    <div className='flex items-center  rounded-md'>
-                                        <div className='mx-1'>
-                                            <Image src={product.image} width={48} height={48} alt='img' />
-                                        </div>
-                                        <p className=''>{product.name.slice(0, 20)}
-                                        </p>
-                                        {/* <p>
+  const selectedData = useSelector(
+    (state) => state.persistedPcBuilderReducer.selectedProduct
+  );
+  // console.log("selected data", selectedData);
+  // console.log("categoryData", categoryData);
+  const handleCompleteBuild = () =>
+    toast("Build is completed successfully", {
+      hideProgressBar: true,
+      autoClose: 2000,
+      type: "success",
+      closeButton: false,
+    });
+  return (
+    <div className="">
+      <h1 className="text-center text-xl font-semibold my-5">Components</h1>
+      <div className="flex gap-2 flex-col mx-1">
+        {categoryData &&
+          categoryData.map((category, index) => (
+            <div
+              key={index}
+              className="parent  border border-gray-400 rounded-sm p-2 w-full  md:w-[50%]  mx-auto flex justify-between overflow-auto"
+            >
+              {/* category logo and name */}
+              <div
+                className="flex gap-1 max-w-md max-h-16"
+                style={{ minWidth: "170px" }}
+              >
+                <Image src={category.image} width={48} height={48} alt="img" />
+                <h5>{category.name}</h5>
+              </div>
+              <div>
+                {selectedData.map((product, index) => (
+                  <div key={index}>
+                    {product.category === category.name ? (
+                      <>
+                        <div className="flex items-center  rounded-md">
+                          <div className="mx-1">
+                            <Image
+                              src={product.image}
+                              width={48}
+                              height={48}
+                              alt="img"
+                            />
+                          </div>
+                          <p className="">{product.name.slice(0, 20)}</p>
+                          {/* <p>
                                             {product.price}
                                         </p> */}
-                                    </div></> : null}</div>
-                            ))}
                         </div>
+                      </>
+                    ) : null}
+                  </div>
+                ))}
+              </div>
 
-                        {/* </div> */}
-                        <Link href={`/pc/build/${category.id}`}>
-                            <button className='btn btn-outline'>Select</button>
-                        </Link>
-                    </div >
-                    // console.log(category.image)
-                ))
-                }
+              {/* </div> */}
+              <Link href={`/pc/build/${category.id}`}>
+                <button className="btn btn-outline">Select</button>
+              </Link>
             </div>
-            <button disabled={selectedData.length < 7} onClick={handleCompleteBuild} className='btn btn-outline block mx-auto my-3'>Complete build</button>
-        </div>
-
-    );
+            // console.log(category.image)
+          ))}
+      </div>
+      <button
+        disabled={selectedData.length < 7}
+        onClick={handleCompleteBuild}
+        className="btn btn-outline block mx-auto my-3"
+      >
+        Complete build
+      </button>
+    </div>
+  );
 }
 PcBuild.getLayout = function getLayout(page) {
-    return (
-        <RootLayout>
-            {page}
-        </RootLayout>
-    );
+  return <RootLayout>{page}</RootLayout>;
 };
 export const getServerSideProps = async () => {
-    const res = await fetch(`https://build-master-pro.vercel.app/api/categories`);
-    const data = await res.json();
-    // console.log("Dataaaa", data);
+  const res = await fetch(`https://build-master-pro.vercel.app/api/categories`);
+  const data = await res.json();
+  // console.log("Dataaaa", data);
 
-    return {
-        props: {
-            categoryData: data.data,
-        }
-    };
+  return {
+    props: {
+      categoryData: data.data,
+    },
+  };
 };
